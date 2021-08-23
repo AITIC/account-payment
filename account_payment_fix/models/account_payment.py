9725f459d744880b0e56fbc59ebbf86b6ac3223a
Katia Logas <klogas@aitic.com.ar>
2021-08-23 10:22:45 -0300

    Volver a commit anterior.

--- account_payment_fix/models/account_payment.py
+++ account_payment_fix/models/account_payment.py
@@ -9,6 +9,7 @@ _logger = logging.getLogger(__name__)
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
+    # state = fields.Selection(track_visibility='always')
     amount = fields.Monetary(currency_field='currency_id', track_visibility='always')
     partner_id = fields.Many2one(
         comodel_name='res.partner',
@@ -25,6 +26,11 @@ class AccountPayment(models.Model):
                                   track_visibility='always',
                                   compute=False)
 
+    # amount = fields.Monetary(track_visibility='always')
+    # partner_id = fields.Many2one(track_visibility='always')
+    # journal_id = fields.Many2one(track_visibility='always')
+    # destination_journal_id = fields.Many2one(track_visibility='always')
+    # currency_id = fields.Many2one(track_visibility='always')
     # campo a ser extendido y mostrar un nombre detemrinado en las lineas de
     # pago de un payment group o donde se desee (por ej. con cheque, retención,
     # etc)
