0ec1e5027cefc05d08eda062e644cd92dfabb072
Sergio Oropeza <67077115+sergioropeza@users.noreply.github.com>
2024-05-23 15:39:06 -0400

    Update account_check.py
    bug fix _prepare_payment_moves

--- account_check/models/account_check.py
+++ account_check/models/account_check.py
@@ -483,9 +483,9 @@ class AccountCheck(models.Model):
         parecido a los statements donde odoo ya lo genera posteado
         """
         # payment.post()
-        move = self.env['account.move'].with_context(default_type='entry').create(payment._prepare_payment_moves())
-        move.post()
-        payment.write({'state': 'posted', 'move_name': move.name})
+        # move = self.env['account.move'].with_context(default_type='entry').create(payment._prepare_payment_moves())
+        # move.post()
+        payment.write({'state': 'posted'})
 
     def handed_reconcile(self, move):
         """
